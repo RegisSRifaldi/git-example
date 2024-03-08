79bfe492789a96debe8647c7f50addb99ec299a1
Regis <regisrifaldi8@gmail.com>
2024-03-08 21:46:33 +0700

    feat: add pengurangan function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.info("Hello World!");
+function penjumlahan() {
+  if (isNaN(a) || isNaN(b)) throw new Error("must be a number");
+}
+
+console.info(penjumlahan(5 + 5));
+
+function pengurangan() {
+  if (isNaN(a) || isNaN(b)) throw new Error("must be a number");
+}
+
+console.info(penjumlahan(10 - 5));
